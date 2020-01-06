48df4af664c0de45b936d1db09ca130bb1ca57a1
赵小亮 <1287520001@qq.com>
2020-01-06 16:31:28 +0800

    update:home顶部右侧

--- src/views/components/home-top-header/HomeTopHeader.vue
+++ src/views/components/home-top-header/HomeTopHeader.vue
@@ -1,8 +1,26 @@
 <template>
   <div class="home-top-header">
     <div class="left">
-      <i :class="['iconfont',leftAsideCollapse?' el-icon-s-unfold':'el-icon-s-fold']" @click="changeCollapse"></i>
+      <i
+        :class="['iconfont',leftAsideCollapse?' el-icon-s-unfold':'el-icon-s-fold']"
+        @click="changeCollapse"
+      ></i>
       <p>toptoptop</p>
+    </div>
+    <div class="right">
+      <el-dropdown>
+        <div class="right-info">
+          <span>哈哈哈哈哈哈</span>
+          <el-avatar
+            size="medium"
+            src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+          ></el-avatar>
+        </div>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>个人中心</el-dropdown-item>
+          <el-dropdown-item>退出</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
     </div>
   </div>
 </template>
@@ -34,12 +52,24 @@ export default {
 .home-top-header {
   display: flex;
   justify-content: space-between;
+  align-items: center;
   .left {
     display: flex;
     align-items: center;
     i {
       font-size: 30px;
       width: 40px;
+    }
+  }
+  .right {
+    .right-info {
+      height: 36px;
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+      span {
+        margin-right: 12px;
+      }
     }
   }
 }
