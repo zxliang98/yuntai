b3f9542f8e0a64599a70f3b87bb38ee8dd5f7b36
赵小亮 <1287520001@qq.com>
2020-01-20 17:41:42 +0800

    update:为登录表单添加校验规则

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,29 +1,160 @@
 <template>
   <div class="login">
     <div class="bg-img"></div>
-    <el-card shadow="always" class="login-card">
+    <el-card v-show="isLogin" shadow="always" class="login-card" key="login-card">
       <h3 class="title">登录</h3>
-      <el-form ref="ruleForm" label-width="60px" class="demo-ruleForm">
-        <el-form-item label="手机号" prop="name">
-          <el-input></el-input>
+      <el-form :model="loginInfo" :rules="loginRules" ref="loginForm" label-width="70px">
+        <el-form-item label="手机号" prop="mobile">
+          <el-input v-model="loginInfo.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item label="密码" prop="name">
-          <el-input></el-input>
+        <el-form-item label="密码" prop="passward">
+          <el-input v-model="loginInfo.passward" show-password placeholder="请输入密码"></el-input>
         </el-form-item>
         <el-form-item label-width="0">
-          <el-button class="login-button" type="primary">登录</el-button>
+          <el-button @click="login('loginForm')" class="login-button" type="primary">登录</el-button>
         </el-form-item>
       </el-form>
       <div class="register">
         <span>还没有账号?去</span>
-        <span class="to-register">注册</span>
+        <span @click="changeMode" class="to-register">注册</span>
+      </div>
+    </el-card>
+    <el-card v-show="!isLogin" shadow="always" class="login-card" key="register-card">
+      <h3 class="title">注册</h3>
+      <el-form :model="registerInfo" :rules="registerRules" ref="registerForm" label-width="80px">
+        <el-form-item label="邀请码" prop="code">
+          <el-input v-model="registerInfo.code" placeholder="请输入邀请码"></el-input>
+        </el-form-item>
+        <el-form-item label="手机号" prop="mobile">
+          <el-input v-model="registerInfo.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="passward">
+          <el-input v-model="registerInfo.passward" show-password placeholder="请设置密码"></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="confirmPassward">
+          <el-input v-model="registerInfo.confirmPassward" show-password placeholder="请确认密码"></el-input>
+        </el-form-item>
+        <el-form-item label-width="0">
+          <el-button @click="register('registerForm')" class="login-button" type="primary" plain>注册</el-button>
+        </el-form-item>
+      </el-form>
+      <div class="register">
+        <span>已经有账号?去</span>
+        <span @click="changeMode" class="to-register">登录</span>
       </div>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    var checkConfirmPassward = (rule, value, callback) => {
+      if (value !== this.registerInfo.passward) {
+        callback(new Error('密码输入不一致'))
+      }
+    }
+    return {
+      isLogin: true,
+      loginInfo: {
+        mobile: '',
+        passward: ''
+      },
+      registerInfo: {
+        code: '',
+        mobile: '',
+        passward: '',
+        confirmPassward: ''
+      },
+      loginRules: {
+        mobile: [
+          {
+            required: true,
+            message: '请输入手机号',
+            trigger: 'blur'
+          },
+          {
+            pattern: /^1[34578]\d{9}$/,
+            message: '手机号格式错误',
+            trigger: 'blur'
+          }
+        ],
+        passward: [
+          {
+            required: true,
+            message: '请输入密码',
+            trigger: 'blur'
+          },
+          {
+            pattern: /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,16}$/,
+            message: '密码需由8-16位字母数字组成',
+            trigger: 'blur'
+          }
+        ]
+      },
+      registerRules: {
+        code: [
+          { required: true, message: '请输入邀请码', trigger: 'blur' },
+          {
+            pattern: /^[a-z0-9]+$/i,
+            message: '邀请码格式错误',
+            trigger: 'blur'
+          }
+        ],
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          {
+            pattern: /^1[34578]\d{9}$/,
+            message: '手机号格式错误',
+            trigger: 'blur'
+          }
+        ],
+        passward: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          {
+            pattern: /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,16}$/,
+            message: '密码需由8-16位字母数字组成',
+            trigger: 'blur'
+          }
+        ],
+        confirmPassward: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { validator: checkConfirmPassward, trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    login (formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          console.log(this.loginInfo)
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    register (formName) {
+      console.log(this.registerInfo)
+      this.$refs[formName].validate(valid => {
+        console.log(valid)
+
+        if (valid) {
+          console.log(22222222222222222)
+
+          console.log(this.registerInfo)
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    changeMode (form) {
+      this.isLogin = !this.isLogin
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
@@ -50,8 +181,8 @@ export default {}
     }
   }
   .login-card {
-    width: 400px;
-    height: 300px;
+    // width: 400px;
+    // height: 300px;
     background-color: rgba(255, 255, 255, 0.8);
     position: absolute;
     top: 50%;
